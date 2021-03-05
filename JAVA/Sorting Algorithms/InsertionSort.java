f46beef1a5fcfc06ce5a32128bbedcd6f21433d7
water1995 <genevievexal@hotmail.com>
2021-03-05 22:53:54 +0530

    5/3/2021

---
+++ JAVA/Sorting Algorithms/InsertionSort.java
@@ -0,0 +1,25 @@
+class InsertionSort{
+
+  public static void main(String[] args) {
+    
+    int[] a = {36,19,29,12,5};
+    int temp,j;
+
+    for(int i=1 ; i < a.length ; i++){
+
+      temp = a[i];
+      j=i;
+      while( j > 0 && a[j-1] > temp){
+
+        a[j] = a[j-1];
+        j = j-1;
+
+      }
+      a[j] = temp;
+    }
+    for(int k=0 ; k < a.length ; k++){
+
+      System.out.print(a[k]+" ");
+    }
+  }
+}
